be3403ddd65f15a2422971f3e62e6e9b09327c37
VadimDu <llihyp@mail.ru>
2022-05-29 20:48:47 +0300

    update 29.05.22 v6.0

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
 from pytest import fixture
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 @fixture()
 def browser():
-    browser = webdriver.Chrome("C:/Chromedriver/chromedriver.exe")
+    browser = webdriver.Chrome(service=Service("C:/Chromedriver/chromedriver.exe"))
     yield browser
     browser.close()
